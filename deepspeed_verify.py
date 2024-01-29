f4b3ba64b5fa00120a7bcc57459befd525f85249
Zhuoming Chen <zhuominc@lovelace.ece.local.cmu.edu>
2024-01-29 01:20:36 -0500

    deepspeed

---
+++ deepspeed_verify.py
@@ -0,0 +1,55 @@
+import torch
+from transformers import LlamaForCausalLM
+import argparse
+import time
+import deepspeed
+#from Llama import LlamaForCausalLM_Attn
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', type=str, default="meta-llama/Llama-2-13b-hf",help='model')
+parser.add_argument('--T', type=int, default=100, help='repeat times')
+parser.add_argument('--B', type=int, default=1, help='batch size')
+parser.add_argument('--P', type=int, default=128, help='prefix length')
+args = parser.parse_args()
+print(args)
+
+#target_model = LlamaForCausalLM_Attn.from_pretrained(args.target, torch_dtype=torch.float16, device_map="auto")
+draft_model = LlamaForCausalLM.from_pretrained(args.model)
+
+
+draft_model = deepspeed.init_inference(draft_model,  
+                dtype=torch.float16, replace_with_kernel_inject=True)
+T = args.T
+B = args.B
+P = args.P
+LEN = [args.P]
+prefix = torch.randint(low=3, high=30000, size=(B, P)).cuda()
+draft_model(input_ids = prefix, use_cache=True)
+
+
+PERFORMANCE = []
+
+for l in LEN:
+
+    sentence = torch.randint(low=3, high=30000, size=(B,  l)).cuda()
+    total_time = 0.0
+    for _ in range(3):
+        output = draft_model(input_ids = sentence, use_cache=True)
+    torch.cuda.synchronize()
+    t1 = time.time()
+    for _ in range(T):
+        output = draft_model(input_ids = sentence, use_cache=True)
+    torch.cuda.synchronize()
+    t2 = time.time()
+    total_time += (t2 - t1)
+    PERFORMANCE.append(total_time / T)
+
+for i, l in enumerate(LEN):
+    print("Length :{}, inference time:{}".format(l, PERFORMANCE[i]))
+
+
+
+
+
+
+
+
--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
-bash benchmark.sh >> benchmark.log
+python deepspeed_verify.py --P 32 >> deepspeed.log
+python deepspeed_verify.py --P 64 >> deepspeed.log
+python deepspeed_verify.py --P 128 >> deepspeed.log
+python deepspeed_verify.py --P 192 >> deepspeed.log
+python deepspeed_verify.py --P 256 >> deepspeed.log
 
-CUDA_VISIBLE_DEVICES=0 python testbed.py --model  JackFram/llama-68m  --target meta-llama/Llama-2-13b-hf  --T 0.01 --P 1.0 --B 128  --DP 0.99 --W 32 --start 0 --end 10 --Mode baseline --M  384 --dataset ./dataset/c4_small.json >> benchmark.log
-
-CUDA_VISIBLE_DEVICES=0 python testbed.py --model  JackFram/llama-68m  --target meta-llama/Llama-2-13b-hf  --T 0.01 --P 1.0 --B 128  --DP 0.99 --W 32 --start 0 --end 10 --Mode greedy --M  384 --dataset ./dataset/c4_small.json >> benchmark.log
-
-CUDA_VISIBLE_DEVICES=0 python testbed.py --model  JackFram/llama-68m  --target meta-llama/Llama-2-13b-hf  --T 0.01 --P 1.0 --B 128  --DP 0.99 --W 32 --start 0 --end 10 --Mode benchmark --M  384 --dataset ./dataset/c4_small.json >> benchmark.log
