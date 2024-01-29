842e3ce79e73057a77ef3a3feefac992fdbf08ec
Zhuoming Chen <zhuominc@lovelace.ece.local.cmu.edu>
2024-01-29 11:09:13 -0500

    offloading

--- benchmark.sh
+++ benchmark.sh
@@ -1,15 +1,15 @@
-CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 1
+CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 1 --offloading
 
-CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 2
+CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 2 --offloading
 
-CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 4
+CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 4 --offloading
 
-CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 8
+CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 8 --offloading
 
-CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 16
+CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 16 --offloading
 
-CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 32
+CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 32 --offloading
 
-CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 64
+CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 64 --offloading
 
-CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 128
+CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 128 --offloading
