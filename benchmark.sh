2f19f30218f8fe23e70ab984b72c885678bb3cdf
Zhuoming Chen <zhuominc@lovelace.ece.local.cmu.edu>
2024-01-29 22:08:46 -0500

    benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -1,30 +1,18 @@
-CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 1 --offloading
+CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 1
 
-CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 2 --offloading
+CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 2 
 
-CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 4 --offloading
+CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 4 
 
-CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 8 --offloading
+CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 8 
 
-CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 16 --offloading
+CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 16 
 
-CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 32 --offloading
+CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 32 
 
-CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 64 --offloading
+CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 64 
 
-CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 128 --offloading
+CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 128 
 
-CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 256 --offloading
-
-CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 512 --offloading
-
-CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 1024 --offloading
-
-CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 1536 --M 2048 --offloading
-
-CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 2048 --M 2560 --offloading
-
-CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 2560 --M 4096 --offloading
-
-CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 3120 --M 4096 --offloading
+CUDA_VISIBLE_DEVICES=0 python benchmark_inference.py --P 128 --D 256 
 
